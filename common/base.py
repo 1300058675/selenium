34c6348c80201d1786167c2478afa05ed4bc3b01
1300058675 <13990164894@163.com>
2019-04-24 22:37:32 +0800

    修改config

--- common/base.py
+++ common/base.py
@@ -7,21 +7,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from common.log import Log
 from readconfig import ReadConfig
 
-
 logger = Log()
 read_config = ReadConfig()
 
 url = read_config.getValue('testServer', 'URL')
 browserName = read_config.getValue('browserType', 'browserName')
 
-
 class Base(object):
 
-    def __init__(self, driver = browserName, base_url = url):
-        self.driver = driver
+    def __init__(self, browser = browserName, base_url = url):
+        try:
+            if browser == "Firefox" or browser =="firefox":
+                driver = webdriver.Firefox()
+                self.driver = driver
+                logger.info("正在打开"+browser+"浏览器")
+            elif browser == "Chrome" or browser == "chrome":
+                driver = webdriver.Chrome()
+                self.driver = driver
+                logger.info("正在打开"+browser+"浏览器")
+            elif browser == "ie" or browser =="IE":
+                driver = webdriver.Ie()
+                self.driver = driver
+                logger.info("正在打开"+browser+"浏览器")
+            elif browser == "phantomjs":
+                driver = webdriver.PhantomJS()
+                self.driver = driver
+                logger.info("正在打开"+browser+"浏览器")
+            else:
+                logger.error("没有发现你所使用的浏览器,你可以使用'firefox','chrome', 'ie' 或者 'phantomjs'")
+        except Exception as e: # 捕获所有异常的方方法
+            logger.error("%s" % e)
+
         self.base_url = base_url
         self.timeout =15
         self.t = 0.5
+
 
     def open(self):
         '''
@@ -30,6 +50,8 @@ class Base(object):
             driver.open()
         '''
         self.driver.get(self.base_url)
+        logger.info('open the url  '+self.base_url)
+
 
     def find_element(self, locator):
         try:
@@ -40,6 +62,7 @@ class Base(object):
         except Exception as msg:  # 捕获所有异常的方方法
             logger.error('“'+locator[0]+'”----“'+locator[1]+'”,定位失败')
 
+
     def find_elements(self, locator):
         try:
             logger.info('正在通过==》“'+locator[0]+'”定位元素==》“'+locator[1]+'”')
@@ -48,6 +71,7 @@ class Base(object):
             return element
         except Exception as msg:  # 捕获所有异常的方方法
             logger.error('“'+locator[1]+'”这组元素定位失败')
+
 
     def sendKeys(self, locator, text):
         element = self.find_element(locator)
@@ -63,6 +87,11 @@ class Base(object):
         element = self.find_element(locator)
         element.clear()
 
+    def max_window(self):
+        self.driver.maximize_window()
+        logger.info('max the window')
+
+
     def is_title(self, title):
         '''判断标题(完全)，返回bool值'''
         try:
@@ -70,6 +99,7 @@ class Base(object):
             return result
         except:
             logger.error('URL的标题不一致')
+
 
     def is_title_contains(self, title):
         '''判断标题（包含），返回bool值'''
@@ -79,6 +109,7 @@ class Base(object):
         except:
             logger.error('URL的标题不包含'+str(title))
 
+
     def is_ElementExist(self, locator):
         '''判定元素是否存在'''
         try:
@@ -87,6 +118,7 @@ class Base(object):
         except:
             logger.error('元素“'+locator[1]+'”不存在')
 
+
     def is_text_element(self, locator, text):
         '''判定某个元素中的text的字符是否和预期一致'''
         try:
@@ -94,6 +126,7 @@ class Base(object):
             return result
         except:
             logger.error('元素“'+locator[1]+'”中的text和预期结果不一致')
+
 
     def is_Selected(self, locator):
         '''判断下拉框或者按钮元素是否被选中，返回bool值   例如：判断全选按钮是哦夫选中'''
@@ -104,6 +137,7 @@ class Base(object):
         except:
             logger.error('元素“' + locator[1] + '”未被选中')
 
+
     def get_text(self, locator):
         '''获取元素文本'''
         try:
@@ -112,6 +146,7 @@ class Base(object):
         except:
             logger.error('获取元素“'+locator[1]+'”文本值失败，将输出一个空字符')
             return ''
+
 
     def get_value(self,locator):
         '''获取文本框内容'''
@@ -122,35 +157,36 @@ class Base(object):
             logger.info('获取元素“'+locator[1]+'”文本值失败，将输出一个空字符')
             return ''
 
+
     def scroll_bar_bottom(self):
         # 滑动滚动条到底部
         js = "window.scrollTo(0, document.body.scrollHeight)"
-        driver.execute_script(js)
+        self.driver.execute_script(js)
 
 
     def scroll_bar_top(self):
         js = "window.scrollTo(0,0)"  # 外滑动到顶部
-        driver.execute_script(js)
+        self.driver.execute_script(js)
 
 
     def scroll_bar_left(self):
         js = "window.scrollTo(0,0)"  # 外滑动到最左端
-        driver.execute_script(js)
+        self.driver.execute_script(js)
 
 
     def scroll_bar_right(self):
         js = 'window.scrollTo(document.body.scrollWidth,0)' # 外滑动滚动条到最右端
-        driver.execute_script(js)
+        self.driver.execute_script(js)
 
 
     def scroll_bar_Specified_element(self, locator):
         js = "arguments[0].scrollIntoView();"  # 外滑动滚动条到某个指定的元素"
         Specified_element = self.find_element(locator)
-        driver.execute_script(js, Specified_element)
+        self.driver.execute_script(js, Specified_element)
 
     def iframe_frame(self, locator):  # 切换iframe
         iframe = self.find_element(locator)  # 获取iframe
-        driver.switch_to.frame(iframe)  # 切换至body
+        self.driver.switch_to.frame(iframe)  # 切换至body
 
     # def window_handles(self):
     #     all = self.driver.window_handles  # 获取全部页面
@@ -159,15 +195,15 @@ class Base(object):
     #     print(current)
 
     def switch_to_window(self, n):
-        all = driver.window_handles
-        driver.switch_to_window(all[n])  # 切换页面
+        all = self.driver.window_handles
+        self.driver.switch_to_window(all[n])  # 切换页面
 
     def get_tag(self, locator):
-        tag = self.find_element(locator).tag_name#获取元素的标签
+        tag = self.find_element(locator).tag_name  # 获取元素的标签
         return tag
 
-    def alert(self):#弹窗
-        a=driver.switch_to_alert()
+    def alert(self):  # 弹窗
+        a = self.driver.switch_to_alert()
         return a
 
     def is_alert(self):
@@ -186,20 +222,20 @@ class Base(object):
         except:
             return False
 
-    def alert_text(self, text):# 弹窗  text  确定  取消
-        driver.switch_to_alert().send_keys(text)
+    def alert_text(self, text):  # 弹窗  text  确定  取消
+        self.driver.switch_to_alert().send_keys(text)
 
 
-    def mouse_flight(self, locator):#鼠标悬停
+    def mouse_flight(self, locator):  # 鼠标悬停
         mouse = self.find_element(locator)
-        ActionChains(driver).move_to_element(mouse).perform()
+        ActionChains(self.driver).move_to_element(mouse).perform()
 
-    def double_click(self, locator):#鼠标双击
+    def double_click(self, locator):  # 鼠标双击
         mouse = self.find_element(locator)
-        ActionChains(driver).double_click(mouse).perform()
+        ActionChains(self.driver).double_click(mouse).perform()
 
 
-    def check_box(self, locator, m, n):#复选框
+    def check_box(self, locator, m, n):  # 复选框
         '''
         all=driver.find_elements_by_xpath(".//*[@type='checkbox']")
         for i in all:
@@ -209,13 +245,13 @@ class Base(object):
         for i in all[m:n]:
             i.click()
 
-    def File_upload(self,text):#文件上传,test传文件路径（.au3转换的.exe）
+    def File_upload(self,text):  # 文件上传,test传文件路径（.au3转换的.exe）
         relesefile = text
         os.system(relesefile)
 
-    def readonly(self, locator):#针对Id
+    def readonly(self, locator):  # 针对Id
         js = "document.getElementBy"+locator[0].title()+"('"+locator[1]+"').removeAttribute('readonly');"
-        driver.execute_script(js)
+        self.driver.execute_script(js)
 
     def drag_and_drop(self, el_locator, ta_locator):
         '''
@@ -227,20 +263,12 @@ class Base(object):
         target = self.find_element(ta_locator)
         ActionChains(self.driver).drag_and_drop(element, target).perform()
 
-
 if __name__ == '__main__':
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get('https://www.baidu.com')
-    sleep(2)
-    # locator('id', 'alert')
-    Test = Base(driver)
-    driver.maximize_window()
-    driver.set_window_size(500, 500)
-    sleep(1)
-    l=('id','su')
-    Test.scroll_bar_Specified_element(l)
-
+    base = Base()
+    base.open()
+    base.max_window()
+    base.find_element(('id', 'su'))
+    base.sendKeys(('id', 'kw'), 'haha')
 
 
 
